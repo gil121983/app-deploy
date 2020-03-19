0c92d56e3083c6884c0e958419b029c49c423fa0
gil121983 <gilstolar@icloud.com>
2020-03-19 12:44:37 +0200

    Updates

---
+++ precache-manifest.8c9ff84063947465b7f48c96d772f21d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934dd135ddace586e68d7e047dc1ad4d",
+    "url": "/app-deploy/index.html"
+  },
+  {
+    "revision": "750b3233aacdfcc0f434",
+    "url": "/app-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "554c621562e8337e75b4",
+    "url": "/app-deploy/static/js/2.89ee7b0e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/app-deploy/static/js/2.89ee7b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750b3233aacdfcc0f434",
+    "url": "/app-deploy/static/js/main.4f9c1102.chunk.js"
+  },
+  {
+    "revision": "e74c31e6373c9bc38f26",
+    "url": "/app-deploy/static/js/runtime-main.2c0717eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
